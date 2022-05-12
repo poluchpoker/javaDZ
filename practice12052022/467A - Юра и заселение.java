7187e4e7194bc259fbe695971ed99606ec190ccb
poluchpoker <90266390+poluchpoker@users.noreply.github.com>
2022-05-12 22:05:16 +0300

    Update 467A - Юра и заселение.java

--- practice12052022/467A - Юра и заселение.java
+++ practice12052022/467A - Юра и заселение.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class YuraAndObzhaga {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int k = scanner.nextInt();
