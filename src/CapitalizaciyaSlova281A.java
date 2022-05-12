a3c961e00e877fa08b203af5fa2f4d102835b8f6
poluchpoker <90266390+poluchpoker@users.noreply.github.com>
2022-05-12 11:10:12 +0300

    Update CapitalizaciyaSlova281A.java

--- src/CapitalizaciyaSlova281A.java
+++ src/CapitalizaciyaSlova281A.java
@@ -1,1 +1,14 @@
-
+import java.util.Scanner;
+import java.lang.Math;
+ 
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	
+	String slovo = scanner.nextLine();
+  String a = slovo.charAt(0) + "";
+ 
+  System.out.println(a.toUpperCase() + slovo.substring(1, slovo.length()));
+    }
+}
