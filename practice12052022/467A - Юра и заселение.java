cd3a315032f7939e3a5b29aac9fc555d55361619
poluchpoker <90266390+poluchpoker@users.noreply.github.com>
2022-05-12 13:59:40 +0300

    Create 467A - Юра и заселение.java

---
+++ practice12052022/467A - Юра и заселение.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class YuraAndObzhaga {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt();
+        int count = 0;
+        for (int i = 0; i < k; i++) {
+            int m = scanner.nextInt();
+            int n = scanner.nextInt();
+            if(n - m >= 2){
+                count += 1;
+            }
+        }
+        System.out.println(count);
+    }
+}
